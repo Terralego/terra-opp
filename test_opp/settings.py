9f734e632dac237803f15f508bd4a38b2d38c0bd
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2019-10-07 11:13:52 +0200

    Fix permissions and media_url in tests

--- test_opp/settings.py
+++ test_opp/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'http://testserver/media/'
 SWAGGER_ENABLED = False
 
 # set required DRF settings
