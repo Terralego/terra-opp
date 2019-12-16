b265eceddac82195e2094763a0b594e96293f04e
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-16 15:34:21 +0100

    fix router basename

--- terra_opp/urls.py
+++ terra_opp/urls.py
@@ -7,9 +7,9 @@ app_name = 'terra_opp'
 
 router = SimpleRouter()
 
-router.register(r'viewpoints', views.ViewpointViewSet, base_name='viewpoint')
-router.register(r'campaigns', views.CampaignViewSet, base_name='campaign')
-router.register(r'pictures', views.PictureViewSet, base_name='picture')
+router.register(r'viewpoints', views.ViewpointViewSet, basename='viewpoint')
+router.register(r'campaigns', views.CampaignViewSet, basename='campaign')
+router.register(r'pictures', views.PictureViewSet, basename='picture')
 
 urlpatterns = router.urls
 
