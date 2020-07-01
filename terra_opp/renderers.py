502015b1b5a414e3bce5e00b0a2f98b3acb16756
jmr <jmr@makina-corpus.com>
2020-07-01 14:45:54 +0200

    Change weasyprint url fetcher to match default file storage or minio file storage

--- terra_opp/renderers.py
+++ terra_opp/renderers.py
@@ -27,34 +27,35 @@ class CSVRenderer(renderers.BaseRenderer):
         return csv_file.read()
 
 
-def django_url_fetcher(url):
-    mime_type, encoding = mimetypes.guess_type(url)
-    url_path = urlparse(url).path
-    data = {
-        'mime_type': mime_type,
-        'encoding': encoding,
-        'filename': basename(url_path),
-    }
+def django_url_fetcher(url, *args, **kwargs):
+    if settings.DEFAULT_FILE_STORAGE == 'django.core.files.storage.FileSystemStorage':
+        mime_type, encoding = mimetypes.guess_type(url)
+        url_path = urlparse(url).path
+        data = {
+            'mime_type': mime_type,
+            'encoding': encoding,
+            'filename': basename(url_path),
+        }
 
-    minio_scheme = 'https' if settings.AWS_S3_SECURE_URLS else 'http'
-    minio_url_prefix = f"{minio_scheme}://{settings.AWS_S3_CUSTOM_DOMAIN}"
+        if settings.MEDIA_URL and url_path.startswith(settings.MEDIA_URL):
+            path = url_path.replace(settings.MEDIA_URL, settings.MEDIA_ROOT)
+            data['file_obj'] = default_storage.open(path)
+            return data
 
-    if url.startswith(minio_url_prefix):  # media file from minio
-        url = url.replace(
-            minio_url_prefix,
-            settings.AWS_S3_ENDPOINT_URL + settings.AWS_STORAGE_BUCKET_NAME
-        )
+        elif settings.STATIC_URL and url_path.startswith(settings.STATIC_URL):
+            path = url_path.replace(settings.STATIC_URL, '')
+            data['file_obj'] = open(find(path), 'rb')
+            return data
 
-    elif settings.MEDIA_URL and url_path.startswith(settings.MEDIA_URL):
-        # media file from filesystem
-        path = url_path.replace(settings.MEDIA_URL, settings.MEDIA_ROOT)
-        data['file_obj'] = default_storage.open(path)
-        return data
+    elif settings.DEFAULT_FILE_STORAGE == 'storages.backends.s3boto3.S3Boto3Storage':
+        minio_scheme = 'https' if settings.AWS_S3_SECURE_URLS else 'http'
+        minio_url_prefix = f"{minio_scheme}://{settings.AWS_S3_CUSTOM_DOMAIN}"
 
-    elif url_path.startswith(settings.STATIC_URL):
-        path = url_path.replace(settings.STATIC_URL, '')
-        data['file_obj'] = open(find(path), 'rb')
-        return data
+        if url.startswith(minio_url_prefix):  # media file from minio
+            url = url.replace(
+                minio_url_prefix,
+                settings.AWS_S3_ENDPOINT_URL + settings.AWS_STORAGE_BUCKET_NAME
+            )
 
     # fall back to weasyprint default fetcher
     return weasyprint.default_url_fetcher(url)
