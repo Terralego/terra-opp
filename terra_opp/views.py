a92301d268220c9b6f28d6f9348979abc9f5121b
jmr <jmr@makina-corpus.com>
2020-09-08 18:08:35 +0200

    Add pictures and related documents to prefetch related on Viewpoint queryset

--- terra_opp/views.py
+++ terra_opp/views.py
@@ -101,6 +101,8 @@ class ViewpointViewSet(viewsets.ModelViewSet):
             qs = Viewpoint.objects.all().distinct()
         pictures_qs = Picture.objects.order_by('-created_at')
         return qs.select_related('point', 'city').prefetch_related(
+            'pictures',
+            'related',
             Prefetch('pictures', queryset=pictures_qs, to_attr='_ordered_pics'),
             'themes',
         )
