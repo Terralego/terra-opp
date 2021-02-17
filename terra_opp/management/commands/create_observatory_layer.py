a0a9278a4a226553888cd87eb39110912e545975
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-02-17 11:40:41 +0100

    Enhance command

--- terra_opp/management/commands/create_observatory_layer.py
+++ terra_opp/management/commands/create_observatory_layer.py
@@ -6,37 +6,69 @@ from geostore.models import Layer
 
 
 class Command(BaseCommand):
-    help = _('Create a django-geostore point layer to store observatory viewpoints')
+    help = _("Create a django-geostore point layer to store observatory viewpoints")
 
     def add_arguments(self, parser):
-        parser.add_argument('-n',
-                            '--name',
-                            action="store",
-                            help="Name for layer to create.")
+        parser.add_argument(
+            "-n", "--name", action="store", help="Name for layer to create."
+        )
+        parser.add_argument(
+            "-f",
+            "--force",
+            action="store_true",
+            help="Create even if the TROPP_OBSERVATORY_LAYER_PK already set.",
+        )
 
-    def get_existing_layer(self):
-        existing_observatory = getattr(settings, 'TROPP_OBSERVATORY_LAYER_PK')
-        if existing_observatory:
+    def handle(self, *args, **options):
+        existing_observatory = getattr(settings, "TROPP_OBSERVATORY_LAYER_PK")
+
+        name = options.get("name") or "opp_baselayer"
+        force = options.get("force")
+
+        if existing_observatory and not force:
             try:
                 Layer.objects.get(pk=existing_observatory)
             except Layer.DoesNotExist:
-                raise CommandError('Defined layer with PK "%s" defined in setting TROPP_OBSERVATORY_LAYER_PK does not exists in database' % existing_observatory)
-        return existing_observatory
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"Defined layer with PK {existing_observatory} defined in setting TROPP_OBSERVATORY_LAYER_PK does not exists in database (-f to force createn)"
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"A layer already exists for this TROPP_OBSERVATORY_LAYER_PK: {existing_observatory} - Do nothing"
+                    )
+                )
+        else:
+            if force:
+                try:
+                    # whether a layer with this id already exists
+                    Layer.objects.get(pk=existing_observatory)
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f"A layer already exists for this TROPP_OBSERVATORY_LAYER_PK: {existing_observatory} - Do nothing"
+                        )
+                    )
+                    return
+                except Layer.DoesNotExist:
+                    pass
 
-    def handle(self, *args, **options):
-        existing_observatory = self.get_existing_layer()
-        name = options.get('name')
-        if existing_observatory:
+            try:
+                # whether a layer with this name already exists
+                l = Layer.objects.get(name=name)
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"A layer with pk {l} already exists for this name: {name} - Can't create new one."
+                    )
+                )
+                return
+            except Layer.DoesNotExist:
+                pass
+
+            layer = Layer.objects.create(geom_type=GeometryTypes.Point, name=f"{name}")
             self.stdout.write(
-                self.style.WARNING(
-                    f"{_('An existing layer already exists for this:')} - TROPP_OBSERVATORY_LAYER_PK: {existing_observatory}"
+                self.style.SUCCESS(
+                    f"Layer has been created. Please set TROPP_OBSERVATORY_LAYER_PK={layer.pk} in project settings"
                 )
             )
-        else:
-            layer = Layer.objects.create(
-                geom_type=GeometryTypes.Point,
-                name=f"{name}"
-            )
-            self.stdout.write(self.style.SUCCESS(
-                _('Layer has been created. Please set TROPP_OBSERVATORY_LAYER_PK=%s in project settings' % layer.pk)
-            ))
